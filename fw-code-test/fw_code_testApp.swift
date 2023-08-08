43af76cafaa30dd28caea99e1971a0c94a2aa821
Taha Chaudhry <taha.chaudhry@icloud.com>
2023-08-08 17:59:45 +0300

    Initial Commit

---
+++ fw-code-test/fw_code_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  fw_code_testApp.swift
+//  fw-code-test
+//
+//  Created by Taha Chaudhry on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct fw_code_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
